f763338e38345731bba430cb6b2e94877ad62692
lzw <linzowo@qq.com>
2018-05-20 16:59:35 +0800

    可视化后，进行调整前

--- python_repos.py
+++ python_repos.py
@@ -1,6 +1,8 @@
 #_*_ coding:utf_8 _*_
 
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS,LightenStyle as LS
 
 #执行api调用病存储调用信息
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
@@ -14,15 +16,18 @@ print "Total repositories:",response_dict['total_count']
 
 #探索有关仓库的信息
 repo_dicts = response_dict['items']
-print "Repositories returned:",len(repo_dicts)
 
-#研究所有仓库
-print "Slected information about first repository:"
+#获取可视化数据
+names,stars = [],[]
 for repo_dict in repo_dicts:
-    print "\nname:",repo_dict["name"]
-    print "Owner:",repo_dict["owner"]["login"]
-    print "Start:",repo_dict["stargazers_count"]
-    print "Repository:",repo_dict["html_url"]
-    print "Created",repo_dict["created_at"]
-    print "Updated:",repo_dict["updated_at"]
-    print "Description:",repo_dict["description"]
+    names.append(repo_dict["name"])
+    stars.append(repo_dict["stargazers_count"])
+
+#可视化
+my_style = LS('#333366',base_style=LCS)
+chart = pygal.Bar(style=my_style,x_label_rotation=45,show_legend=False)
+chart.title = "Mosr-Starred python projects on GitHub"
+chart.x_labels = names
+
+chart.add("",stars)
+chart.render_to_file('python_repos.svg')
