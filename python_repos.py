8619f343a5c1d550e887f3e3c379d0b11ff4f65a
lzw <linzowo@qq.com>
2018-05-20 17:26:46 +0800

    完成config修改

--- python_repos.py
+++ python_repos.py
@@ -25,7 +25,18 @@ for repo_dict in repo_dicts:
 
 #可视化
 my_style = LS('#333366',base_style=LCS)
-chart = pygal.Bar(style=my_style,x_label_rotation=45,show_legend=False)
+
+my_config = pygal.Config() #创建一个config类的实例
+my_config.x_label_rotation = 45 #将x标签沿着x轴旋转45度
+my_config.show_legend = False  #隐藏图例
+my_config.title_font_size = 24  #标题的大小
+my_config.label_font_size = 14 #副标签的字体大小
+my_config.major_label_font_size = 18  #主标签的字体大小
+my_config.truncate_label = 15 #控制标签的显示长度在15个字节内
+my_config.show_y_guides = False #将y轴的水平线隐藏
+my_config.width = 1000  #设置图表的尺寸
+
+chart = pygal.Bar(my_config,style=my_style)
 chart.title = "Mosr-Starred python projects on GitHub"
 chart.x_labels = names
 
