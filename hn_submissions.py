36ee488568cd5a97984e987f1ede08fc55ba8ca0
linzowo <linzowo@qq.com>
2018-05-26 10:53:00 +0800

    添加hacker news程序

---
+++ hn_submissions.py
@@ -0,0 +1,39 @@
+#_*_ coding:utf_8 _*_
+
+import requests
+
+from operator import itemgetter
+
+#执行api调用并存储响应
+url = 'https://hacker-news.firebaseio.com/v0/topstories.json'
+r = requests.get(url)
+print 'Status code:',r.status_code  #输出
+
+#处理每篇文章的信息
+submission_ids = r.json() #获取一个包含hacker_news网ID的列表
+submission_dicts = []  #创建一个存储热门文章信息的列表
+
+#查询每个ID的具体信息
+for submission_id in submission_ids:
+    #获取每个ID的具体具体信息
+    url = ('https://hacker-news.firebaseio.com/v0/item/'+
+    str(submission_id)+'.json')  #根据id生成相应的api接口
+    submission_r = requests.get(url)
+    print 'Status code:',submission_r.status_code #检查是否链接成功
+    response_dict = submission_r.json()  #生成一个包含ID信息的列表
+    
+    submission_dict = {
+        'title':response_dict['title'],
+        'link':'http://news.ycombinator.com/item?id='+str(submission_id),
+        'comments':response_dict.get('descendants',0),
+    }
+
+    submission_dicts.append(submission_dict)  #将获取的信息存储到列表
+
+submission_dicts = sorted(submission_dicts,key=itemgetter('comments'),reverse=False) #将获取到的类别内容以comments为关键词进行倒序排序
+
+#逐个输出文章的相关信息
+for submission_dict in submission_dicts:
+    print '\nTitle:',submission_dict['title']
+    print 'Discussion link:',submission_dict['link']
+    print 'Comments:',submission_dict['comments']
--- python_repos.py
+++ python_repos.py
@@ -4,6 +4,11 @@ import requests
 import pygal
 from pygal.style import LightColorizedStyle as LCS,LightenStyle as LS
 
+#解决无法处理字符流的问题
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 #执行api调用病存储调用信息
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 
@@ -18,10 +23,17 @@ print "Total repositories:",response_dict['total_count']
 repo_dicts = response_dict['items']
 
 #获取可视化数据
-names,stars = [],[]
+names,plot_dicts = [],[]
 for repo_dict in repo_dicts:
     names.append(repo_dict["name"])
-    stars.append(repo_dict["stargazers_count"])
+
+#添加星星数和简介
+    plot_dict = {
+        'value':repo_dict['stargazers_count'],
+        'label':str(repo_dict['description']),
+        'xlink':repo_dict['html_url'],
+    }
+    plot_dicts.append(plot_dict)
 
 #可视化
 my_style = LS('#333366',base_style=LCS)
@@ -40,5 +52,5 @@ chart = pygal.Bar(my_config,style=my_style)
 chart.title = "Mosr-Starred python projects on GitHub"
 chart.x_labels = names
 
-chart.add("",stars)
+chart.add("",plot_dicts)
 chart.render_to_file('python_repos.svg')
